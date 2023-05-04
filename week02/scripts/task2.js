6196126ebe6a27e022694ed5f190cff61578f1ff
PanamaJeff <cas22030@byui.edu>
2023-05-03 19:25:32 -0500

    assertion

--- week02/scripts/task2.js
+++ week02/scripts/task2.js
@@ -6,16 +6,16 @@
 let name = "Jeffrey Castro";
 
 // Step 2: place the value of the name variable into the HTML file (hint: document.querySelector())
-document.querySelector("task2.html").innerHTML = name;
+document.querySelector("#name").innerHTML = name;
 
 // Step 3: declare and instantiate a variable to hold the current year
 let currentYear = new Date().getFullYear();
 
 // Step 4: place the value of the current year variable into the HTML file
-document.querySelector("task2.html").innerHTML = currentYear;
+// This step is already done in the HTML file, so it can be skipped.
 
 // Step 5: declare and instantiate a variable to hold the name of your picture
-let picture = "myPicture.jpg";
+let picture = "Me.jpg";
 
 // Step 6: copy your image into the "images" folder
 // (This step should be done manually, outside of the script)
@@ -30,7 +30,7 @@ document.querySelector("img").setAttribute("src", "images/" + picture);
 let favoriteFoods = ["pizza", "pasta", "ice cream"];
 
 // Step 2: place the values of the favorite foods variable into the HTML file
-document.querySelector("task2.html").innerHTML = favoriteFoods.join(", ");
+document.querySelector("#favoriteFoods").innerHTML = favoriteFoods.join(", ");
 
 // Step 3: declare and instantiate a variable to hold another favorite food
 let anotherFavoriteFood = "sushi";
@@ -39,16 +39,16 @@ let anotherFavoriteFood = "sushi";
 favoriteFoods.push(anotherFavoriteFood);
 
 // Step 5: repeat Step 2
-document.querySelector("task2.html").innerHTML = favoriteFoods.join(", ");
+document.querySelector("#favoriteFoods").innerHTML = favoriteFoods.join(", ");
 
 // Step 6: remove the first element in the favorite foods array
 favoriteFoods.shift();
 
 // Step 7: repeat Step 2
-document.querySelector("task2.html").innerHTML = favoriteFoods.join(", ");
+document.querySelector("#favoriteFoods").innerHTML = favoriteFoods.join(", ");
 
 // Step 8: remove the last element in the favorite foods array
 favoriteFoods.pop();
 
 // Step 9: repeat Step 2
-document.querySelector("task2.html").innerHTML = favoriteFoods.join(", ");
+document.querySelector("#favoriteFoods").innerHTML = favoriteFoods.join(", ");
