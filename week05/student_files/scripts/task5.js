b550b2c73dff80c9d6faf834216312d0a5581c92
PanamaJeff <cas22030@byui.edu>
2023-05-20 21:54:28 -0500

    task 5

--- week05/student_files/scripts/task5.js
+++ week05/student_files/scripts/task5.js
@@ -1,101 +1,87 @@
-// Step 1, 2, 3
+/* IF/ELSE IF */
 let currentDate = new Date();
 let dayOfWeek = currentDate.getDay();
+let message = '';
 
-// Step 4
-let message1;
-
-// Step 5
-if (dayOfWeek >= 1 && dayOfWeek <= 5) {
-  message1 = 'Hang in there!';
+if (dayOfWeek > 0 && dayOfWeek < 6) {
+    message = 'Hang in there!';
 } else {
-  message1 = 'Woohoo! It is the weekend!';
+    message = 'Woohoo! It is the weekend!';
 }
 
-// Step 6
-let message2;
+/* SWITCH, CASE, BREAK */
+let dayMessage = '';
 
 switch (dayOfWeek) {
-  case 0:
-    message2 = 'Sunday';
-    break;
-  case 1:
-    message2 = 'Monday';
-    break;
-  case 2:
-    message2 = 'Tuesday';
-    break;
-  case 3:
-    message2 = 'Wednesday';
-    break;
-  case 4:
-    message2 = 'Thursday';
-    break;
-  case 5:
-    message2 = 'Friday';
-    break;
-  case 6:
-    message2 = 'Saturday';
-    break;
+    case 0:
+        dayMessage = 'Sunday';
+        break;
+    case 1:
+        dayMessage = 'Monday';
+        break;
+    case 2:
+        dayMessage = 'Tuesday';
+        break;
+    case 3:
+        dayMessage = 'Wednesday';
+        break;
+    case 4:
+        dayMessage = 'Thursday';
+        break;
+    case 5:
+        dayMessage = 'Friday';
+        break;
+    case 6:
+        dayMessage = 'Saturday';
+        break;
+    default:
+        dayMessage = 'Invalid day';
 }
 
-// Step 7
-document.getElementById('message1').innerHTML = message1;
-document.getElementById('message2').innerHTML = message2;
+/* OUTPUT */
+document.getElementById('message1').textContent = message;
+document.getElementById('message2').textContent = dayMessage;
 
-// Step 8
+/* FETCH */
 let templeList = [];
 
-async function output(temples) {
-  let templesDiv = document.getElementById('temples');
-  templesDiv.innerHTML = '';
+function output(temples) {
+    const parentElement = document.getElementById('temples');
+    temples.forEach(temple => {
+        let article = document.createElement('article');
+        let h3 = document.createElement('h3');
+        let h4_1 = document.createElement('h4');
+        let h4_2 = document.createElement('h4');
+        let img = document.createElement('img');
 
-  temples.forEach(temple => {
-    let article = document.createElement('article');
+        h3.textContent = temple.templeName;
+        h4_1.textContent = temple.location;
+        h4_2.textContent = temple.dedicated;
+        img.src = temple.imageUrl;
+        img.alt = temple.templeName;
 
-    let h3 = document.createElement('h3');
-    h3.textContent = temple.templeName;
-
-    let h4_1 = document.createElement('h4');
-    h4_1.textContent = temple.location;
-
-    let h4_2 = document.createElement('h4');
-    h4_2.textContent = temple.dedicated;
-
-    let img = document.createElement('img');
-    img.src = temple.imageUrl;
-    img.alt = temple.templeName;
-
-    article.appendChild(h3);
-    article.appendChild(h4_1);
-    article.appendChild(h4_2);
-    article.appendChild(img);
-
-    templesDiv.appendChild(article);
-  });
+        article.append(h3, h4_1, h4_2, img);
+        parentElement.append(article);
+    });
 }
 
 async function getTemples() {
-  let response = await fetch('https://byui-cse.github.io/cse121b-course/week05/temples.json');
-  templeList = await response.json();
-  output(templeList);
+    const response = await fetch('https://byui-cse.github.io/cse121b-course/week05/temples.json');
+    templeList = await response.json();
+    output(templeList);
 }
 
 getTemples();
 
 function reset() {
-  document.getElementById('temples').innerHTML = '';
+    document.getElementById('temples').innerHTML = '';
 }
 
 function sortBy() {
-  reset();
-  
-  let sortValue = document.getElementById('sortBy').value;
-  let sortedList = [...templeList];
-
-  sortedList.sort((a, b) => (a[sortValue] > b[sortValue]) ? 1 : -1);
-
-  output(sortedList);
+    reset();
+    let sortByValue = document.getElementById('sortBy').value;
+    let sortedTemples = templeList.sort((a, b) => a[sortByValue].localeCompare(b[sortByValue]));
+    output(sortedTemples);
 }
 
 document.getElementById('sortBy').addEventListener('change', sortBy);
